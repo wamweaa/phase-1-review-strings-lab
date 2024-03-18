3da47ecc65211c10b9535640fc630e28f1d37c0c
wamwea <patokinya12@gmail.com>
2024-03-19 02:01:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+// const welcomeMessage ='Welcome to Flatbook , Grace Hopper!'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `'WELCOME TO FLATBOOK, '${'GRACE HOPPER'}!`;
+'Grace Hopper'.charAt(0);
+const shortGreeting = `'Welcome, '${'Grace Hopper'.charAt(0)} !`;
